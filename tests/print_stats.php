ca4ba14323e4ea4c7a7bac7bfdccac54a884bbcf
Tony Martin <tony.martin@astehelsinki.fi>
2017-12-29 19:49:30 +0200

    Stats

---
+++ tests/print_stats.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ .'/../vendor/autoload.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$cache = new Crusse\ShmCache();
+print_r( $cache->getStats() );
+
